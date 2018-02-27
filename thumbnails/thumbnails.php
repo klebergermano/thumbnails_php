9922cad19405068c7f29f27aa314a9fa62bbc143
Kleberus <kleber_lsgermano@hotmail.com>
2018-02-27 04:42:47 -0300

    inicio

---
+++ thumbnails/thumbnails.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Cria um Thumbnail nas pasta img do projeto, caso não haja essa 
+   pasta ela sera criada automaticamente
+ 
+ // @author Kleber Germano <Kleberus> kleber_lsgermano@hotmail.com
+ */
+class thumbnails {
+    
+}
